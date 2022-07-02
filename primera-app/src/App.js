09933a46dd0fb5f41ade63a373f848a41b2c1683
Nicolas Benjamin Cruz Carpio <nicolasbncruz@hotmail.com>
2022-07-01 23:55:59 -0500

    add mi primera app

---
+++ primera-app/src/App.js
@@ -0,0 +1,17 @@
+const App=()=> (  
+    <div className="App">
+      <header className="App-header">
+        <h1> Bienvenidos a EDTeam</h1>                
+        <a
+          className="App-link"
+          href="https://ed.team"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visita nuestra web
+        </a>
+      </header>
+    </div>   
+  );
+
+export default App;
